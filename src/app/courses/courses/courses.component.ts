315e1f38572296cec4fa7b6aefb5ef7733adebbe
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-18 19:47:17 -0300

    Iniciando módulo de serviço

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,17 +9,19 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    {_id: '1', name: 'Angular 15', category: 'Front-End'}
-  ];
+  courses: Course[] = []
   displayedColumns = ['name', 'category'];
 
-  constructor(){
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService){
     //this.courses = [];
+    //this.coursesService = new CoursesService();
+    //this.courses = this.coursesService.findAll();
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.courses = this.coursesService.findAll();
   }
 
 }
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { Course } from './../model/course';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private httpClient : HttpClient) { }
+
+
+  findAll(): Course[]{
+
+    return [
+      {_id: '1', name: 'Angular 15', category: 'Front-End'}
+    ];
+
+  }
+}

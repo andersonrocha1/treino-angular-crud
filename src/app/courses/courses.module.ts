8d18bab6c6a63b1d0ca576143f654935a08b9748
Anderson Rocha <anderson.tec.informacao@gmail.com>
2024-01-04 14:25:43 -0300

    Migração Standalone com exclusão de módulos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
-import { SharedModule } from '../shared/shared.module';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
@@ -14,14 +14,12 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent,
-        CourseFormComponent,
-        CoursesListComponent
-    ]
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent,
+    CourseFormComponent,
+    CoursesListComponent
+]
 })
 export class CoursesModule { }
